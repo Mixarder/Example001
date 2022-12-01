b9cfb76196d27c2f772597f63bc72cfe8f2aa992
Mixarder <tarbitme@gmail.com>
2022-12-01 17:22:10 +0300

    add_condition_If_else

---
+++ Example005_ConditionifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿ Console.Write("Ведите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "рита")
+ {
+    Console.WriteLine("Привет, родная!");
+ }
+ else
+ {
+    Console.Write("Ну, заходи, ");
+    Console.WriteLine(username);
+ }
